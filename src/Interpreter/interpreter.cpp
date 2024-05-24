15f302b61cd0390660b8b8f1296ff8d48f1f7d04
Yuxin-Qin <y.qin.1@research.gla.ac.uk>
2024-05-24 18:19:29 +0100

    debug for the last change on cheri

--- src/Interpreter/interpreter.cpp
+++ src/Interpreter/interpreter.cpp
@@ -1,3 +1,18 @@
+#include <cheri/cheric.h>
+// Undefine conflicting macros before including cheriintrin.h
+#undef CHERI_PERM_GLOBAL
+#undef CHERI_PERM_EXECUTE
+#undef CHERI_PERM_LOAD
+#undef CHERI_PERM_STORE
+#undef CHERI_PERM_LOAD_CAP
+#undef CHERI_PERM_STORE_CAP
+#undef CHERI_PERM_STORE_LOCAL_CAP
+#undef CHERI_PERM_SEAL
+#undef CHERI_PERM_UNSEAL
+#undef CHERI_PERM_SYSTEM_REGS
+#undef CHERI_PERM_SYSTEM
+#include <cheriintrin.h>
+
 #include "interpreter.h"
 
 #include <cmath>
@@ -134,23 +149,27 @@ bool Interpreter::store(Module *m, uint8_t type, uint32_t addr,
 
 #endif /* !defined(__CHERI_PURE_CAPABILITY__) */
 
-#if defined(__CHERI_PURE_CAPABILITY__)
-    void *bounded_mem;
-
+#if !defined(__CHERI_PURE_CAPABILITY__)
+    void * __capability bounded_mem;
+    
     // Create a capability for the memory range and set bounds
     bounded_mem = cheri_bounds_set(m->memory.bytes, m->memory.pages * (uint32_t)PAGE_SIZE);
 
+    // Convert maddr to ptraddr_t for the check
+    ptraddr_t maddr_addr = (ptraddr_t)maddr;
+    ptraddr_t maddr_size_addr = (ptraddr_t)(maddr + size);
+
     // Check if maddr is within the bounds
-    if (!cheri_is_address_inbounds(bounded_mem, maddr)) {
+    if (!cheri_is_address_inbounds(bounded_mem, maddr_addr)) {
         overflow = true;
     }
 
     // Check if maddr + size is within the bounds
-    if (!cheri_is_address_inbounds(bounded_mem, maddr + size)) {
+    if (!cheri_is_address_inbounds(bounded_mem, maddr_size_addr)) {
         overflow = true;
     }
 
-#endif /* defined(__CHERI_PURE_CAPABILITY__) */
+#endif /* !defined(__CHERI_PURE_CAPABILITY__) */
 
     if (!m->options.disable_memory_bounds) {
         if (overflow) {
