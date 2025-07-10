7720bcf09c3bc5c1b0497213fce2127e11b555ff
Yuxin-Qin <77993247+Yuxin-Qin@users.noreply.github.com>
2025-07-10 14:31:43 +0800

    Update emulated.cpp

--- src/Primitives/emulated.cpp
+++ src/Primitives/emulated.cpp
@@ -1,18 +1,7 @@
+#include "../Interpreter/instructions.h"
 #ifndef ARDUINO
 
-/**
- * This file lists the primitives of the language and stores them in the
- * primitives
- *
- * Adding a primitive:
- *  1) Bump up the NUM_PRIMITIVES constant
- *  2) Define <primitive>_type
- *  3) Define a function void primitive(Module* m)
- *  4) Extend the install_primitives function
- *
- */
 #include <sys/time.h>
-
 #include <chrono>
 #include <cmath>
 #include <cstdio>
@@ -24,29 +13,9 @@
 #include "../Utils/util.h"
 #include "../WARDuino/CallbackHandler.h"
 #include "primitives.h"
-#include "../Interpreter/instructions.h"
-
-#define pop_args(n) m->sp -= n
-#define get_arg(m, arg) m->stack[(m)->sp - (arg)].value
-#define pushUInt32(arg) m->stack[++m->sp].value.uint32 = arg
-#define pushInt32(arg) m->stack[++m->sp].value.int32 = arg
-#define pushUInt64(arg)                 \
-    m->stack[++m->sp].value_type = I64; \
-    m->stack[m->sp].value.uint64 = arg
-#define arg0 get_arg(m, 0)
-#define arg1 get_arg(m, 1)
-#define arg2 get_arg(m, 2)
-#define arg3 get_arg(m, 3)
-#define arg4 get_arg(m, 4)
-#define arg5 get_arg(m, 5)
-#define arg6 get_arg(m, 6)
-#define arg7 get_arg(m, 7)
-#define arg8 get_arg(m, 8)
-#define arg9 get_arg(m, 9)
 
 #define NUM_PRIMITIVES 0
 #define NUM_PRIMITIVES_ARDUINO 29
-
 #define ALL_PRIMITIVES (NUM_PRIMITIVES + NUM_PRIMITIVES_ARDUINO)
 
 // Global index for installing primitives
@@ -54,12 +23,10 @@ int prim_index = 0;
 
 double sensor_emu = 0;
 
-static int interpreter_running = 1;  // 1 means running, 0 means exit
-static int exit_code = 0;            // Stores the exit code when `exit_w` is called
+static int interpreter_running = 1;
+static int exit_code = 0;
 
-/*
-   Private macros to install a primitive
-*/
+// === Macros for defining and using primitives
 #define install_primitive(prim_name)                                       \
     {                                                                      \
         dbg_info("installing primitive number: %d  of %d with name: %s\n", \
@@ -95,21 +62,6 @@ static int exit_code = 0;            // Stores the exit code when `exit_w` is ca
     void function_name##_serialize(       \
         std::vector<IOStateElement *> &external_state)
 
-def_prim(exit_vm, oneToNoneU32) {
-    debug("EMU: exit_w called with exit code ");
-    printf("Exit code: %u\n", arg0.uint32);  // Print exit code passed as argument
-    exit(arg0.uint32);  // Use the exit code passed in to exit
-    pop_args(1);  // Pop the exit code argument
-}
-
-def_prim(exit_w, oneToNoneU32) {
-    pop_args(1);           // Pop the exit code argument
-    exit_code = arg0.uint32;  // Set the exit code for reference
-    interpreter_running = 0;  // Signal the interpreter to stop runnin
-    return true;
-}
-
-// TODO: use fp
 #define pop_args(n) m->sp -= n
 #define get_arg(m, arg) m->stack[(m)->sp - (arg)].value
 #define pushUInt32(arg) m->stack[++m->sp].value.uint32 = arg
@@ -128,7 +80,135 @@ def_prim(exit_w, oneToNoneU32) {
 #define arg8 get_arg(m, 8)
 #define arg9 get_arg(m, 9)
 
-// The primitive table
+// === Argument type arrays
+uint32_t param_arr_len0[0] = {};
+uint32_t param_I32_arr_len1[1] = {I32};
+uint32_t param_I32_arr_len2[2] = {I32, I32};
+uint32_t param_I32_arr_len3[3] = {I32, I32, I32};
+uint32_t param_I32_arr_len4[4] = {I32, I32, I32, I32};
+uint32_t param_I32_arr_len10[10] = {I32, I32, I32, I32, I32,
+                                    I32, I32, I32, I32, I32};
+uint32_t param_I64_arr_len1[1] = {I64};
+
+// === Type definitions (MUST be declared before def_prim)
+Type oneToNoneU32 = {
+    .form = FUNC,
+    .param_count = 1,
+    .params = param_I32_arr_len1,
+    .result_count = 0,
+    .results = nullptr,
+    .mask = 0x8001
+};
+
+Type twoToNoneU32 = {
+    .form = FUNC,
+    .param_count = 2,
+    .params = param_I32_arr_len2,
+    .result_count = 0,
+    .results = nullptr,
+    .mask = 0x80011
+};
+
+Type threeToNoneU32 = {
+    .form = FUNC,
+    .param_count = 3,
+    .params = param_I32_arr_len3,
+    .result_count = 0,
+    .results = nullptr,
+    .mask = 0x800111
+};
+
+Type fourToNoneU32 = {
+    .form = FUNC,
+    .param_count = 4,
+    .params = param_I32_arr_len4,
+    .result_count = 0,
+    .results = nullptr,
+    .mask = 0x8001111
+};
+
+Type oneToOneU32 = {
+    .form = FUNC,
+    .param_count = 1,
+    .params = param_I32_arr_len1,
+    .result_count = 1,
+    .results = param_I32_arr_len1,
+    .mask = 0x80011
+};
+
+Type oneToOneI32 = {
+    .form = FUNC,
+    .param_count = 1,
+    .params = param_I32_arr_len1,
+    .result_count = 1,
+    .results = param_I32_arr_len1,
+    .mask = 0x80011
+};
+
+Type twoToOneU32 = {
+    .form = FUNC,
+    .param_count = 2,
+    .params = param_I32_arr_len2,
+    .result_count = 1,
+    .results = param_I32_arr_len1,
+    .mask = 0x81011
+};
+
+Type threeToOneU32 = {
+    .form = FUNC,
+    .param_count = 3,
+    .params = param_I32_arr_len3,
+    .result_count = 1,
+    .results = param_I32_arr_len1,
+    .mask = 0x810111
+};
+
+Type fourToOneU32 = {
+    .form = FUNC,
+    .param_count = 4,
+    .params = param_I32_arr_len4,
+    .result_count = 1,
+    .results = param_I32_arr_len1,
+    .mask = 0x8101111
+};
+
+Type tenToOneU32 = {
+    .form = FUNC,
+    .param_count = 10,
+    .params = param_I32_arr_len10,
+    .result_count = 1,
+    .results = param_I32_arr_len1,
+    .mask = 0x8101111111111
+};
+
+Type NoneToNoneU32 = {
+    .form = FUNC,
+    .param_count = 0,
+    .params = nullptr,
+    .result_count = 0,
+    .results = nullptr,
+    .mask = 0x80000
+};
+
+Type NoneToOneU32 = {
+    .form = FUNC,
+    .param_count = 0,
+    .params = nullptr,
+    .result_count = 1,
+    .results = param_I32_arr_len1,
+    .mask = 0x81000
+};
+
+Type NoneToOneU64 = {
+    .form = FUNC,
+    .param_count = 0,
+    .params = nullptr,
+    .result_count = 1,
+    .results = param_I64_arr_len1,
+    .mask = 0x82000
+};
+
+// === Primitive table
 PrimitiveEntry primitives[ALL_PRIMITIVES];
 
 //
